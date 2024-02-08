0209de571fb492f9a3cc579887eef44cbaf2f2e3
Alex <lesha.rudiko@gmail.com>
2024-02-08 21:24:47 +0300

    load new lesson

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+const title = ''
+const screenPrice = ''
+const rollback = ''
+const fullPrice = ''
+
+let adaptive = ''
+let screens = ''
+
+alert('the connection with space is servered.... :X')
+
+console.log('хватит этого Английского, я русский! шутка');
