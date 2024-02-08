579d4837961da36ae86bff9c0ccbb852b865d859
Alex <lesha.rudiko@gmail.com>
2024-02-08 21:29:04 +0300

    update file js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const title = ''
-const screenPrice = ''
-const rollback = ''
-const fullPrice = ''
+const title = "";
+const screenPrice = "";
+const rollback = "";
+const fullPrice = "";
 
-let adaptive = ''
-let screens = ''
+let adaptive = "";
+let screens = "";
 
-alert('the connection with space is servered.... :X')
+alert("the connection with space is servered.... :X");
 
-console.log('хватит этого Английского, я русский! шутка');
+console.log("хватит этого Английского, я русский! шутка");
