1422382e3a685d10b4c7b7a6effa7563d9d897eb
Alex <lesha.rudiko@gmail.com>
2024-02-11 15:37:32 +0300

    new lesson2

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-const title = ''
-const screenPrice = ''
-const rollback = ''
-const fullPrice = ''
+const title = 'newProject'
+const screenPrice = 1500
+const rollback = 20
+const fullPrice = 70000
 
-let adaptive = ''
-let screens = ''
+let adaptive = true
+let screens = 'Простые, Сложные, Интерактивные'
 
-alert('the connection with space is servered.... :X')
+console.log(typeof title);
+console.log(typeof screens);
+console.log(screenPrice + ' рублей')
+console.log(fullPrice + '$')
+console.log(screens.toLocaleLowerCase().split());
+console.log(fullPrice * (rollback / 100));
 
-console.log('хватит этого Английского, я русский! шутка');
