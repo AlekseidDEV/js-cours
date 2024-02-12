1d5d777d70119630587b97c75439920ebc811594
Alex <lesha.rudiko@gmail.com>
2024-02-12 19:31:31 +0300

    update

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ console.log(typeof adaptive);
 
 console.log(screens.length);
 
-console.log("Стоимость верстки экранов " + screenPrice + " рублей / долларов / гривен / юани")
-console.log("Стоимость разработки сайта " + fullPrice + " рублей / долларов / гривен / юани")
+console.log(`Стоимость верстки экранов\n${screenPrice} \nрублей / долларов / гривен / юани`)
+console.log(`Стоимость разработки сайта ${fullPrice} рублей / долларов / гривен / юани`)
 
 console.log(screens.toLowerCase().split());
 console.log(fullPrice * (rollback / 100));
