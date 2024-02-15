994842af9d5718302b4b64f3aaf990f1fb109133
Alex <lesha.rudiko@gmail.com>
2024-02-15 17:23:04 +0300

    mew lesson

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const title = prompt('Как называется твой проект', 'Project')
+const title = prompt('Как называется твой проект', '   КаЛьКулятор Верстки')
 const screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные')
 const screenPrice = +prompt('Сколько будет стоить данная работа')
 const adaptive = confirm('Нужен ли адаптив ?')
@@ -11,25 +11,58 @@ const servicePrice2 = +prompt('Сколько это будет стоить')
 
 const rollback = 20
 
-const fullPrice = servicePrice1 + servicePrice2 + screenPrice
-const servicePricePercent = fullPrice - ((fullPrice / 100) * rollback)
+let allServicePrices = 0
+let fullPrice = 0
+let servicePricePercent = 0
 
-/* 
-не очень пока что понял, зачем нужно здесь округление, 
-но наверное это будет выглядить вот так: 
-
-const servicePricePercent = Math.ceil(fullPrice - ((fullPrice / 100) * rollback))
-*/
-
-if(fullPrice > 30000){
-    console.log('Даем скидку 10%');
-} else if(fullPrice > 15000 && fullPrice <= 30000){
-    console.log('Даем скидку 5%');
-} else if(fullPrice <= 15000 && fullPrice > 0){
-    console.log('Скидка не предусмотрена');
-} else if(fullPrice < 0){
-    console.log('Что то пошло не так');
+const showTypeOf = function(variable){
+    console.log(variable, typeof variable);
 }
+
+const getAllServicePrice = function(service1, service2){
+    return service1 + service2
+}
+
+function getFullPrice(screenPrice, sumService){
+    return screenPrice + sumService
+}
+
+const getTitle = function(){
+    const trimStr = title.trim()
+
+    return trimStr.split('')[0].toUpperCase() + trimStr.slice(1).toLowerCase()
+}
+
+const getServicePercentPrices = function(fullPrice, rollBack){
+
+    return fullPrice - ((fullPrice / 100) * rollBack)
+}
+
+const getRollBackMessage = function(price){
+    if(price > 30000){
+      return 'Даем скидку 10%'
+    } else if(price > 15000 && price <= 30000){
+       return 'Даем скидку 5%'
+    } else if(price <= 15000 && price > 0){
+        return 'Скидка не предусмотрена'
+    } else if(price < 0){
+       return 'Что то пошло не так'
+    }
+}
+
+showTypeOf(title)
+showTypeOf(adaptive)
+showTypeOf(screens)
+
+allServicePrices = getAllServicePrice(servicePrice1, servicePrice2)
+fullPrice = getFullPrice(screenPrice, allServicePrices)
+getTitle()
+servicePricePercent = getServicePercentPrices(fullPrice, rollback)
+
+console.log(getRollBackMessage(fullPrice));
+
+
+
 
 
 
